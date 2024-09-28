1a0879cbc461cbab9fea3e38cfbba51ecc7cd5b7
minthuw885 <thuw882005@gmail.com>
2024-09-28 19:17:22 +0700

    code

--- Layout_View/app/src/main/java/mthu/baitap/layout_view/MainActivity.java
+++ Layout_View/app/src/main/java/mthu/baitap/layout_view/MainActivity.java
@@ -1,6 +1,10 @@
 package mthu.baitap.layout_view;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,36 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
+    // Tìm điểu khiện để tham chiếu đến và thao tác
+    EditText dieukhienA = findViewById(R.id.editA);
+
+    EditText dieukhienB = findViewById(R.id.editB);
+
+    Button nutTinhToan = findViewById(R.id.btnTinhTong);
+
+    //2. Gắn bộ lắng nghe sự khiện cho điều khiển
 }
+
+public void XuLyTinhTong(View v){
+    //1. Tìm điểu khiển cần thao tác nếu chưa tìm
+    EditText dieukhienA = findViewById(R.id.editA);
+
+    EditText dieukhienB = findViewById(R.id.editB);
+    TextView tvKetQua = findViewById(R.id.tvKetQua);
+    //2. Lấy dữ liệu
+    String strSoThuNhat = dieukhienA.getText().toString();
+    // phải đưa về String, vì getText() cho ta 1 Objext
+    String strSoThuHai = dieukhienB.getText().toString();
+    //3. Đưa về kiểu dữ liệu phù hợp ( ở dây là dữ liệu số) để tính toán
+
+    double soThuNhat = Double.parseDouble(strSoThuNhat);
+    double soThuHai = Double.parseDouble(strSoThuHai);
+    //4. Thực hiện tính toán
+    double ketQua = soThuNhat + soThuHai;
+    //5. Hiển thị kết quả
+    // đưa sang dạng chuỗi
+    String chuoiThongBao = String.valueOf(ketQua);
+    tvKetQua.setText(chuoiThongBao);
+
+}
+    }
