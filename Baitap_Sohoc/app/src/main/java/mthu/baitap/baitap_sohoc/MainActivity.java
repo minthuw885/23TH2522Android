972fc99d9654f1856e08b61fe875d29923d72b47
minthuw885 <thuw882005@gmail.com>
2024-09-28 20:39:10 +0700

    code

--- Baitap_Sohoc/app/src/main/java/mthu/baitap/baitap_sohoc/MainActivity.java
+++ Baitap_Sohoc/app/src/main/java/mthu/baitap/baitap_sohoc/MainActivity.java
@@ -1,6 +1,11 @@
 package mthu.baitap.baitap_sohoc;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,61 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Ánh xạ các thành phần trong giao diện
+        EditText dieuKhienA = findViewById(R.id.edtA);
+        EditText dieuKhienB = findViewById(R.id.edtB);
+        Button nutTinhCong = findViewById(R.id.btnAdd);
+        Button nutTinhTru = findViewById(R.id.btnSub);
+        Button nutTinhNhan = findViewById(R.id.btnMul);
+        Button nutTinhChia = findViewById(R.id.btnDiv);
+        // TextView ketQua = findViewById(R.id.tvResult);
+    }
+        public void XuLy_Cong(View v) {
+            EditText dieukhienA = findViewById(R.id.edtA);
+            EditText dieukhienB = findViewById(R.id.edtB);
+            TextView tvKetQua = findViewById(R.id.tvResult);
+            String strSoThuNhat = dieukhienA.getText().toString();
+            String strSoThuHai = dieukhienB.getText().toString();
+            double soThuNhat = Double.parseDouble(strSoThuNhat);
+            double soThuHai = Double.parseDouble(strSoThuHai);
+            double ketQua = soThuNhat + soThuHai;
+            String chuoiThongBao = String.valueOf(ketQua);
+            tvKetQua.setText(chuoiThongBao);
+        }
+    public void XuLy_Tru(View v) {
+        EditText dieukhienA = findViewById(R.id.edtA);
+        EditText dieukhienB = findViewById(R.id.edtB);
+        TextView tvKetQua = findViewById(R.id.tvResult);
+        String strSoThuNhat = dieukhienA.getText().toString();
+        String strSoThuHai = dieukhienB.getText().toString();
+        double soThuNhat = Double.parseDouble(strSoThuNhat);
+        double soThuHai = Double.parseDouble(strSoThuHai);
+        double ketQua = soThuNhat - soThuHai;
+        String chuoiThongBao = String.valueOf(ketQua);
+        tvKetQua.setText(chuoiThongBao);
+    }
+    public void XuLy_Nhan(View v){
+        EditText dieukhienA = findViewById(R.id.edtA);
+        EditText dieukhienB = findViewById(R.id.edtB);
+        TextView tvKetQua = findViewById(R.id.tvResult);
+        String strSoThuNhat = dieukhienA.getText().toString();
+        String strSoThuHai = dieukhienB.getText().toString();
+        double soThuNhat = Double.parseDouble(strSoThuNhat);
+        double soThuHai = Double.parseDouble(strSoThuHai);
+        double ketQua = soThuNhat * soThuHai;
+        String chuoiThongBao = String.valueOf(ketQua);
+        tvKetQua.setText(chuoiThongBao);
+    }
+    public void XuLy_Chia(View v){
+        EditText dieukhienA = findViewById(R.id.edtA);
+        EditText dieukhienB = findViewById(R.id.edtB);
+        TextView tvKetQua = findViewById(R.id.tvResult);
+        String strSoThuNhat = dieukhienA.getText().toString();
+        String strSoThuHai = dieukhienB.getText().toString();
+        double soThuNhat = Double.parseDouble(strSoThuNhat);
+        double soThuHai = Double.parseDouble(strSoThuHai);
+        double ketQua = soThuNhat / soThuHai;
+        String chuoiThongBao = String.valueOf(ketQua);
+        tvKetQua.setText(chuoiThongBao);
     }
 }
