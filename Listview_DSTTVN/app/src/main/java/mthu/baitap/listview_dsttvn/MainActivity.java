a6079eb323ad741c7fcde6bea920d58158aa18c6
minthuw885 <thuw882005@gmail.com>
2024-09-30 19:56:45 +0700

    code

--- Listview_DSTTVN/app/src/main/java/mthu/baitap/listview_dsttvn/MainActivity.java
+++ Listview_DSTTVN/app/src/main/java/mthu/baitap/listview_dsttvn/MainActivity.java
@@ -1,8 +1,11 @@
 package mthu.baitap.listview_dsttvn;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> dsTenTinhThanhVN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,17 +27,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-            ArrayList<String> dsTenTinhThanhVN;
-            dsTenTinhThanhVN = new ArrayList<String>();
-            dsTenTinhThanhVN.add("Hà Nội");
-            dsTenTinhThanhVN.add("Thành phố Hồ Chí Minh");
-            dsTenTinhThanhVN.add("Đồng Nai");
-            dsTenTinhThanhVN.add("Bình Thuận");
-            dsTenTinhThanhVN.add("Ninh Thuận");
-            dsTenTinhThanhVN.add("Nha Trang");
-            ArrayAdapter<String> adapterTinhThanh;
-            adapterTinhThanh = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dsTenTinhThanhVN);
-            ListView lvTenTinhThanh = findViewById(R.id.lvDSTTVN);
-            lvTenTinhThanh.setAdapter(adapterTinhThanh);
+        dsTenTinhThanhVN = new ArrayList<String>();
+        dsTenTinhThanhVN.add("Hà Nội");
+        dsTenTinhThanhVN.add("Thành phố Hồ Chí Minh");
+        dsTenTinhThanhVN.add("Đồng Nai");
+        dsTenTinhThanhVN.add("Bình Thuận");
+        dsTenTinhThanhVN.add("Ninh Thuận");
+        dsTenTinhThanhVN.add("Nha Trang");
+        ArrayAdapter<String> adapterTinhThanh;
+        adapterTinhThanh = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dsTenTinhThanhVN);
+        ListView lvTenTinhThanh = findViewById(R.id.lvDSTTVN);
+        lvTenTinhThanh.setAdapter(adapterTinhThanh);
+        lvTenTinhThanh.setOnItemClickListener(BoLangNghevaXL);
     }
+    AdapterView.OnItemClickListener BoLangNghevaXL = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            String strTenTinhThanh = dsTenTinhThanhVN.get(i);
+            Toast.makeText(MainActivity.this,strTenTinhThanh, Toast.LENGTH_LONG).show();
+        }
+    };
 }
